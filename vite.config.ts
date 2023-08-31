23b7c86cb88c60b7b7ef8eb9b6bc8e4b3ea4dc12
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-08-31 19:42:52 +0300

    add @rollup/plugin-node-resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
-import scss from 'rollup-plugin-scss';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import scss from "rollup-plugin-scss";
+import resolve from "@rollup/plugin-node-resolve";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), scss()],
-})
+  plugins: [
+    svgr(),
+    react(),
+    scss(),
+    resolve({
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+    }),
+  ],
+});
