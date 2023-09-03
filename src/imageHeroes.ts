393acadcfd92bc210d4a291a1f959f647ba55829
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-09-03 12:41:34 +0300

    create sepatate array for hero images

---
+++ src/imageHeroes.ts
@@ -0,0 +1,63 @@
+import imagePath1def from "./assets/png/pic1_def.png";
+import imagePath2def from "./assets/png/pic2_def.png";
+import imagePath1var2 from "./assets/png/pic1_var2.png";
+import imagePath2var2 from "./assets/png/pic2_var2.png";
+import imagePath1var3 from "./assets/png/pic1_var3.png";
+import imagePath2var3 from "./assets/png/pic2_var3.png";
+import imagePath1var4 from "./assets/png/pic1_var4.png"; 
+import imagePath2var4 from "./assets/png/pic2_var4.png";
+import imagePath1var5 from "./assets/png/pic1_var5.png";
+import imagePath2var5 from "./assets/png/pic2_var5.png";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const heroVar1 = [
+  {
+    path: imagePath1def,
+    name: "Legendary Artifact",
+    level: "Legendary",
+    totalGames: 189,
+    gamesWon: 125,
+    price: 2.5,
+  },
+  {
+    path: imagePath1var2,
+    name: "Epic Warrior",
+    level: "Rare",
+    totalGames: 134,
+    gamesWon: 100,
+    price: 1.5,
+  },
+  {
+    path: imagePath1var3,
+    name: "Mystic Spellbook",
+    level: "Common",
+    totalGames: 133,
+    gamesWon: 123,
+    price: 1.5,
+  },
+  {
+    path: imagePath1var4,
+    name: "Common Potion",
+    level: "Heroic",
+    totalGames: 87,
+    gamesWon: 43,
+    price: 1.2,
+  },
+  {
+    path: imagePath1var5,
+    name: "Heroic Blade",
+    level: "Epic",
+    totalGames: 56,
+    gamesWon: 15,
+    price: 2.8,
+  },
+];
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const heroVar2 = [
+  { path: imagePath2def },
+  { path: imagePath2var2 },
+  { path: imagePath2var3 },
+  { path: imagePath2var4 },
+  { path: imagePath2var5 },
+];
