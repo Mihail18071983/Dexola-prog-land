6da3f432ab92e2a34178676c31a46be679415a48
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-09-05 09:39:19 +0300

    add to header menu smooth apearing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
-import React, { FC, MutableRefObject } from "react";
+import React, { FC, MutableRefObject, memo } from "react";
+import { animated, useSpring } from "@react-spring/web";
+
 import styles from "./Header.module.scss";
 import containerStyles from "../../Container.module.scss";
+
 import { Logo } from "../../shared/svgComponents/Logo";
 import { Link } from "react-router-dom";
 import { Button } from "../../shared/Button/Button";
@@ -11,33 +14,39 @@ interface HeaderProps {
   isAnimating: boolean;
 }
 
-export const Header: FC<HeaderProps> = ({
-  headerRef,
-  scrollToJoinUs,
-  isAnimating,
-}) => {
-  return (
-    <header ref={headerRef} className={styles.header}>
-      <div className={containerStyles.container}>
-        {!isAnimating && (
-          <div className={styles.nav}>
-            <Link
-              to="https://dexola.com"
-              aria-label="logo dx"
-              className={styles.header_logo}
-            >
-              <Logo width="35" height="20" className={styles.icon} />
-            </Link>
-            <Button
-              type="button"
-              className={styles.link}
-              onClick={scrollToJoinUs}
-            >
-              Join Now
-            </Button>
-          </div>
-        )}
-      </div>
-    </header>
-  );
-};
+export const Header: FC<HeaderProps> = memo(
+  ({ headerRef, scrollToJoinUs, isAnimating }) => {
+    const navSpring = useSpring({
+      delay: 5000,
+      from: {
+        opacity: 0,
+      },
+      to: { opacity: 1 },
+      config: { duration: 500 },
+    });
+    return (
+      <header ref={headerRef} className={styles.header}>
+        <div className={containerStyles.container}>
+          {!isAnimating && (
+            <animated.div style={navSpring} className={styles.nav}>
+              <Link
+                to="https://dexola.com"
+                aria-label="logo dx"
+                className={styles.header_logo}
+              >
+                <Logo width="35" height="20" className={styles.icon} />
+              </Link>
+              <Button
+                type="button"
+                className={styles.link}
+                onClick={scrollToJoinUs}
+              >
+                Join Now
+              </Button>
+            </animated.div>
+          )}
+        </div>
+      </header>
+    );
+  }
+);
