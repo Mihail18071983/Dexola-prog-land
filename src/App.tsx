2aadf64d47a9d00ee0adbcb748206f997855099c
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-10-12 18:18:15 +0300

    add button up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-
-import { Main } from "./Pages/Main"
+import { Main } from "./Pages/Main";
+import ScrollToTop from "react-scroll-to-top";
+import { LiaAngleDoubleUpSolid } from "react-icons/lia";
 
 function App() {
-
   return (
     <>
-      <Main/>
+      <Main />
+      <ScrollToTop
+        width="20"
+        height="20"
+        className="up-btn"
+        smooth
+        color="var(--accent-blue)"
+        component={<LiaAngleDoubleUpSolid />}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
